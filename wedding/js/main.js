0bbc77fc83326d05b073561be3ad6c03b1fa8b00
Shreyas Ajay Tigdi <sat2madgo@gmail.com>
2019-11-21 19:58:23 +0530

    Update main.js

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -38,7 +38,28 @@ function scrollHandler(){
     });
       
       // smoothscroll on sidenav click
+ // Add smooth scrolling to all links
+  $(".tabs-content a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+       
+      });
+    } // End if
+  });
+	
     $('.tabgroup > div').hide();
         $('.tabgroup > div:first-of-type').show();
         $('.tabs a').click(function(e){
